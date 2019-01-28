c211481474fec043e2f562be85088f2ee09d2dbb
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-01-28 22:36:04 +0300

    initial

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import Vue from "vue";
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import BaseIcon from "@/components/BaseIcon";
+
+Vue.component("BaseIcon", BaseIcon);
+
+Vue.config.productionTip = false;
+
+// Automatic Global Registration of Base Components
+// https://vuejs.org/v2/guide/components-registration.html#Automatic-Global-Registration-of-Base-Components
+const requireComponent = require.context(
+  "./components",
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName);
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, "$1"))
+  );
+
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
+//
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
